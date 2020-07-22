fcdcad57b7b134d7b8cd14e332911fc455143b4f
Alexandra31150 <alexandrap44@gmail.com>
2020-07-22 16:35:38 +0200

    navbar ok

--- feesmaison/src/components/Navbar.jsx
+++ feesmaison/src/components/Navbar.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-// import './Navbar.css';
+// import {Link} from 'react-router-dom';
+import '../CSS/Navbar.css';
 
 function Navbar() {
   return (
-    <div>
-      <ul className="navbar">
-        <li>
+    <div className="navbar">
+      <ul className="navbar-list">
+        <li className="list">
+          <img src="https://zupimages.net/up/20/30/823v.png" alt="logo" className="logo" /> 
+        </li> 
+        <li className="list">
           Accueil
         </li>
-        <li>
+        <li className="list">
           Créatrices
-        </li><li>
+        </li>
+        <li className="list">
           Contact
         </li>
       </ul>
