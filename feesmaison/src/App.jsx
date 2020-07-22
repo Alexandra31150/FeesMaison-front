b57803da4765938329fa4203f9e13e916de01c10
Alexandra31150 <alexandrap44@gmail.com>
2020-07-22 16:59:37 +0200

    ajout footer

--- feesmaison/src/App.jsx
+++ feesmaison/src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import ContactPage from './components/ContactPage';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <ContactPage />
-      <p className="made">Made with ♡ by Alexandra P.</p>
+      <Footer />
     </div>
   );
 }
---
+++ feesmaison/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+// import {Link} from 'react-router-dom';
+import '../CSS/Footer.css';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <ul className="footer-list">
+        <li className="made">
+        Made with ♡ by Alexandra P.
+        </li>
+        <li className="espace-crea"> 
+          Espace créatrice
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Footer;
